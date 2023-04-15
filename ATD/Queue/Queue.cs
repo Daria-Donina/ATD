ddd1b7fb3b2708c9aab12d0c9187ef897f3c530a
Daria-Donina <donina.daria@yandex.ru>
2023-04-15 17:22:27 +0300

    Enqueue complexity from O(n) to O(1)

--- ATD/Queue/Queue.cs
+++ ATD/Queue/Queue.cs
@@ -4,23 +4,47 @@ namespace ATD.Queue;
 
 public class Queue<T> : IQueue<T>
 {
-    private readonly ILinkedList<T> _items = new SingleLinkedList<T>();
+    private class Node
+    {
+        public T Data { get; private set; }
+        public Node Next { get; set; }
 
-    public void Enqueue(T data) => 
-        _items.Insert(_items.Length, data);
+        public Node(T data, Node next)
+        {
+            Next = next;
+            Data = data;
+        }
+    }
+
+    private Node _head;
+    private Node _tail;
+
+    private bool IsEmpty => _head == null || _tail == null;
+
+    public void Enqueue(T data)
+    {
+        var newNode = new Node(data, null);
+        
+        if (!IsEmpty) 
+            _tail.Next = newNode;
+        else
+            _head = newNode;
+
+        _tail = newNode;
+    }
 
     public T Dequeue()
     {
-        var element = Front();
-        _items.Delete(0);
-        return element;
+        var data = Front();
+        _head = _head.Next;
+        return data;
     }
 
     public T Front()
     {
-        if (_items.Length == 0)
+        if (IsEmpty)
             throw new InvalidOperationException("Queue is empty.");
 
-        return _items.GetItem(0);
+        return _head.Data;
     }
 }
